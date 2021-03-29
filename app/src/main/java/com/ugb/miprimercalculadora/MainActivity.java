a417ce8c898243d1e08ac12925d062695987640b
wilsongutierrez-01 <adolfoguti001@outlook.com>
2021-03-28 18:07:02 -0700

    Errores en la ejecucion

--- app/src/main/java/com/ugb/miprimercalculadora/MainActivity.java
+++ app/src/main/java/com/ugb/miprimercalculadora/MainActivity.java
@@ -3,12 +3,18 @@ package com.ugb.miprimercalculadora;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RadioButton;
@@ -23,12 +29,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    DB miDB;
+    Cursor datosProductosCursos = null;
     FloatingActionButton btn;
     Intent nuevoProducto;
-    ListView ltsAmigos;
-    Cursor datosAmigosCursor = null;
-    ArrayList<productos> amigosArrayList=new ArrayList<productos>();
-    ArrayList<productos> amigosArrayListCopy=new ArrayList<productos>();
+    ListView ltsProductos;
+    ArrayList<productos> productosArrayList=new ArrayList<productos>();
+    ArrayList<productos> productosrrayListCopy=new ArrayList<productos>();
     productos misProductos;
 
 
@@ -40,23 +47,177 @@ public class MainActivity extends AppCompatActivity {
 
         btn = findViewById(R.id.btnAgregarProductos);
         btn.setOnClickListener(v-> {
-            agregarProductos();
-
+            agregarProductos("nuevo", new String[]{});
         } );
-
-
+        obtenerDatosProductos();
+        buscarProductos();
 
     }
-    private void agregarProductos(){
+    //agragando menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+
+        AdapterView.AdapterContextMenuInfo adapterContextMenuInfo = (AdapterView.AdapterContextMenuInfo)menuInfo;
+        datosProductosCursos.moveToPosition(adapterContextMenuInfo.position);
+        menu.setHeaderTitle(datosProductosCursos.getString(1));
+    }
+
+    //Obtener elemtnos selecionados
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
         try {
-            nuevoProducto = new Intent(getApplicationContext(), agregarProducto.class);
-            startActivity(nuevoProducto);
-
-        }catch (Exception e){
-          mostrarMsgToast("Este es el error mensos");
+            switch (item.getItemId()) {
+                case R.id.mnxagregar:
+                    agregarProductos("nuevo", new String[]{});
+                    break;
+                case R.id.mnxmodificar:
+                    String[] datos = {
+                            datosProductosCursos.getString(0),//idProducto
+                            datosProductosCursos.getString(1),//codigo
+                            datosProductosCursos.getString(2),//producto
+                            datosProductosCursos.getString(3),//marca
+                            datosProductosCursos.getString(4),//descripcion
+                            datosProductosCursos.getString(5), //presentacion
+                            datosProductosCursos.getString(6), //precio
+                            datosProductosCursos.getString(7) //UrlImg
+                    };
+                    agregarProductos("modificar", datos);
+                    break;
+                case R.id.mnxeliminar:
+                    eliminarProducto();
+                    break;
+            }
+        }catch (Exception ex){
+            mostrarMsgToast(ex.getMessage());
         }
-
+        return super.onContextItemSelected(item);
     }
+
+    //Eliminar Produtos
+    private void eliminarProducto(){
+        try {
+            AlertDialog.Builder confirmacion = new AlertDialog.Builder(MainActivity.this);
+            confirmacion.setTitle("Esta seguro de eliminar el registro?");
+            confirmacion.setMessage(datosProductosCursos.getString(1));
+            confirmacion.setPositiveButton("Si", (dialog, which) -> {
+                miDB = new DB(getApplicationContext(), "", null, 1);
+                datosProductosCursos = miDB.admin_productos("eliminar", new String[]{datosProductosCursos.getString(0)});//idProducto
+                obtenerDatosProductos();
+                mostrarMsgToast("Registro Eliminado con exito...");
+                dialog.dismiss();//cerrar el cuadro de dialogo
+            });
+            confirmacion.setNegativeButton("No", (dialog, which) -> {
+                mostrarMsgToast("Se cancelo la eliminiacion...");
+                dialog.dismiss();
+            });
+            confirmacion.create().show();
+        }catch (Exception ex){
+            mostrarMsgToast(ex.getMessage());
+        }
+    }
+
+
+//Agregar producto
+    private void agregarProductos(String accion, String[] datos){
+        try {
+            Bundle parametrosProductos = new Bundle();
+            parametrosProductos.putString("accion", accion);
+            parametrosProductos.putStringArray("datos", datos);
+
+            Intent agregarProductos = new Intent(getApplicationContext(), agregarProducto.class);
+            agregarProductos.putExtras(parametrosProductos);
+            startActivity(agregarProductos);
+        }catch (Exception e){
+            mostrarMsgToast(e.getMessage());
+        }
+    }
+    // Obtener datos de Productos
+    private void obtenerDatosProductos(){
+        miDB = new DB(getApplicationContext(),"",null,1);
+        datosProductosCursos = miDB.admin_productos("consultar",null);
+        if( datosProductosCursos.moveToFirst() ){//si hay datos que mostrar
+            mostrarDatosProductos();
+        } else {//sino que llame para agregar nuevos amigos...
+            mostrarMsgToast("No hay datos de amigos que mostrar, por favor agregue nuevos amigos...");
+            agregarProductos("nuevo", new String[]{});
+        }
+    }
+
+    //MOstrar datos productos
+    private void mostrarDatosProductos(){
+        ltsProductos = findViewById(R.id.ltsproducotos);
+        productosArrayList.clear();
+        productosrrayListCopy.clear();
+        do{
+            misProductos = new productos(
+                    datosProductosCursos.getString(0),//idProducto
+                    datosProductosCursos.getString(1),//codigo
+                    datosProductosCursos.getString(2),//producto
+                    datosProductosCursos.getString(3),//marca
+                    datosProductosCursos.getString(4),//descripcion
+                    datosProductosCursos.getString(5), //presentacion
+                    datosProductosCursos.getString(6), //precio
+                    datosProductosCursos.getString(7) //UrlImg
+            );
+            productosArrayList.add(misProductos);
+        }while(datosProductosCursos.moveToNext());
+        adaptadorImagenes adaptadorImagenes = new adaptadorImagenes(getApplicationContext(), productosArrayList);
+        ltsProductos.setAdapter(adaptadorImagenes);
+
+        registerForContextMenu(ltsProductos);
+
+        productosrrayListCopy.addAll(productosArrayList);
+    }
+
+    //Buscar productos
+    private void buscarProductos() {
+        TextView tempVal = findViewById(R.id.txtBuscarProductos);
+        tempVal.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try{
+                    productosArrayList.clear();
+                    if( tempVal.getText().toString().trim().length()<1 ){//si no esta escribiendo, mostramos todos los registros
+                        productosArrayList.addAll(productosrrayListCopy);
+                    } else {//si esta buscando entonces filtramos los datos
+                        for (productos am : productosrrayListCopy){
+                            String producto = am.getProducto();
+                            String codigo = am.getCodigo();
+                            String marca = am.getMarca();
+                            String precio = am.getPrecio();
+
+                            String buscando = tempVal.getText().toString().trim().toLowerCase();//escribe en la caja de texto...
+
+                            if(producto.toLowerCase().trim().contains(buscando) ||
+                                    codigo.trim().contains(buscando) ||
+                                    marca.trim().toLowerCase().contains(buscando) ||
+                                    precio.trim().toLowerCase().contains(buscando)
+                            ){
+                                productosArrayList.add(am);
+                            }
+                        }
+                    }
+                    adaptadorImagenes adaptadorImagenes = new adaptadorImagenes(getApplicationContext(), productosArrayList);
+                    ltsProductos.setAdapter(adaptadorImagenes);
+                }catch (Exception e){
+                    mostrarMsgToast(e.getMessage());
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
 
     //Mensaje Toast
     private void mostrarMsgToast(String msg){
--- app/src/main/java/com/ugb/miprimercalculadora/agregarProducto.java
+++ app/src/main/java/com/ugb/miprimercalculadora/agregarProducto.java
@@ -26,13 +26,19 @@ public class agregarProducto extends AppCompatActivity {
     FloatingActionButton btnAtras;
     ImageView imgFotoProducto;
     Intent tomarFotoIntent;
-    String urlCompletaImg;
+    String urlCompletaImg, idProdcuto, accion="nuevo";
+    Button btn;
+    DB miDB;
+    TextView tempVal;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_productos);
+
+        miDB = new DB (getApplicationContext(),"",null,1);
 
         btnAtras = findViewById(R.id.btnAtras);
         btnAtras.setOnClickListener(v -> {
@@ -43,6 +49,35 @@ public class agregarProducto extends AppCompatActivity {
         imgFotoProducto.setOnClickListener(v -> {
             tomarFotoProducto();
         });
+
+        btn = findViewById(R.id.btnGuardarProducto);
+        btn.setOnClickListener(v->{
+            tempVal = findViewById(R.id.txtCodigo);
+            String codigo = tempVal.getText().toString();
+
+            tempVal = findViewById(R.id.txtNombreProducto);
+            String producto = tempVal.getText().toString();
+
+            tempVal = findViewById(R.id.txtMarca);
+            String marca = tempVal.getText().toString();
+
+            tempVal = findViewById(R.id.txtDescripcion);
+            String descripcion = tempVal.getText().toString();
+
+            tempVal = findViewById(R.id.txtPresentacion);
+            String presentacion = tempVal.getText().toString();
+
+            tempVal = findViewById(R.id.txtPrecio);
+            String precio = tempVal.getText().toString();
+
+            String[] datos = {idProdcuto,codigo,producto,marca,descripcion,presentacion,precio,urlCompletaImg};
+            miDB.admin_productos(accion,datos);
+            mostrarMsgToast("Registro guardado con exito.");
+
+            mostrarVistaPrincipal();
+        });
+        mostrarDatosProductos();
+
 
     }
     //Tomar foto producto
@@ -72,6 +107,42 @@ public class agregarProducto extends AppCompatActivity {
                 mostrarMsgToast("No fue posible tomar la foto");
 
             }
+        }
+    }
+    //Mostras datos productos
+    private void mostrarDatosProductos() {
+        try{
+            Bundle recibirParametros = getIntent().getExtras();
+            accion = recibirParametros.getString("accion");
+            if(accion.equals("modificar")){
+                String[] datos = recibirParametros.getStringArray("datos");
+
+                idProdcuto = datos[0];
+
+                tempVal = findViewById(R.id.txtCodigo);
+                tempVal.setText(datos[1]);
+
+                tempVal = findViewById(R.id.txtNombreProducto);
+                tempVal.setText(datos[2]);
+
+                tempVal = findViewById(R.id.txtMarca);
+                tempVal.setText(datos[3]);
+
+                tempVal = findViewById(R.id.txtDescripcion);
+                tempVal.setText(datos[4]);
+
+                tempVal = findViewById(R.id.txtPresentacion);
+                tempVal.setText(datos[5]);
+
+                tempVal = findViewById(R.id.txtPrecio);
+                tempVal.setText(datos[6]);
+
+                urlCompletaImg = datos[7];
+                Bitmap bitmap = BitmapFactory.decodeFile((urlCompletaImg));
+                imgFotoProducto.setImageBitmap(bitmap);
+            }
+        }catch (Exception e){
+            mostrarMsgToast(e.getMessage());
         }
     }
 
