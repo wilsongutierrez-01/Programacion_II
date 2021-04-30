5cb47b4b1a71d6ffe93d31b8c7de01c11453d819
wilsongutierrez-01 <adolfoguti001@outlook.com>
2021-04-29 21:23:31 -0600

    Inicio couchDB. Creacion de metodos: utilidades internterDetectec sendProducto Modificaciones en Manifest

--- app/src/main/java/com/ugb/miprimercalculadora/MainActivity.java
+++ app/src/main/java/com/ugb/miprimercalculadora/MainActivity.java
@@ -7,9 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -27,7 +29,17 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.MissingFormatArgumentException;
 
 public class MainActivity extends AppCompatActivity {
     DB miDB;
@@ -37,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<productos> productosArrayList = new ArrayList<productos>();
     ArrayList<productos> productosrrayListCopy = new ArrayList<productos>();
     productos misProductos;
+    JSONArray jsonArrayProductos;
+    JSONObject jsonObjectProductos;
+    utilidades u;
+    internterDetectec di;
+    int position = 0;
 
 
 
@@ -45,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        di = new internterDetectec(getApplicationContext());
         btn = findViewById(R.id.btnAgregarProductos);
         try {
             comprobardatos();
@@ -54,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         btn.setOnClickListener(v-> {
-            agregarProductos("nuevo", new String[]{});
+            agregarProductos("nuevo");
         } );
         buscarProducto();
 
@@ -75,22 +92,12 @@ public class MainActivity extends AppCompatActivity {
         try {
             switch (item.getItemId()){
                 case R.id.mnxagregar:
-                    agregarProductos("nuevo", new String[]{});
+                    agregarProductos("nuevo");
                     break;
 
                 case R.id.mnxmodificar:
-                    String [] datos ={
-                            datosProductosCursos.getString(0),
-                            datosProductosCursos.getString(1),
-                            datosProductosCursos.getString(2),
-                            datosProductosCursos.getString(3),
-                            datosProductosCursos.getString(4),
-                            datosProductosCursos.getString(5),
-                            datosProductosCursos.getString(6),
-                            datosProductosCursos.getString(7),
 
-                    };
-                    agregarProductos("modificar",datos);
+                    agregarProductos("modificar");
                     break;
 
                 case R.id.mnxeliminar:
@@ -113,22 +120,38 @@ public class MainActivity extends AppCompatActivity {
     //Eliminar producto
     private void eliminarProdcuto(){
         try {
+            jsonObjectProductos = jsonArrayProductos.getJSONObject(position).getJSONObject("value");
             AlertDialog.Builder confirmacion = new AlertDialog.Builder(MainActivity.this);
             confirmacion.setTitle("¿Seguro desea eliminar?");
-            confirmacion.setMessage(datosProductosCursos.getString(2));
-            confirmacion.setPositiveButton("SI", ((dialog, which) -> {
-                miDB = new DB (getApplicationContext(), "", null, 1);
-                datosProductosCursos =miDB.admin_productos("eliminar", new String[]{datosProductosCursos.getString(0)});
-                comprobardatos();
-                mostrarMsgToast("Eliminado correcto");
+            confirmacion.setMessage(jsonObjectProductos.getString("nombre"));
+            confirmacion.setPositiveButton("SI", ((dialog, which) ->{
+                try {
+                    if (di.internetConnection()){
+                        serverConnection objEliminarprod = new serverConnection();
+                        String resp = objEliminarprod.execute(u.url_mto + jsonObjectProductos.getString("_id") + "?rev" + jsonObjectProductos.getString("_rev"), "DELETE").get();
+                        JSONObject jsonRespDelete = new JSONObject(resp);
+                        if (jsonRespDelete.getBoolean("ok")){
+                            jsonArrayProductos.remove(position);
+                            mostrarProductos();
+                        }
+                    }
+                    miDB = new DB(getApplicationContext(),"",null, 1);
+                    datosProductosCursos = miDB.admin_productos("eliminar", new String[]{jsonObjectProductos.getString("_id")});
+                    comprobardatos();
+                    mostrarMsgToast("Registro eliminado");
+                    dialog.dismiss();
+
+                }catch (Exception e){
+                    mostrarMsgToast(e.getMessage());
+
+                }
+            }
+                    ));
+            confirmacion.setNegativeButton("NO", ((dialog, which) -> {
+                mostrarMsgToast("Cancelando");
                 dialog.dismiss();
             }));
-            confirmacion.setNegativeButton("NO", (dialog, which) -> {
-                mostrarMsgToast("Se cancelo eliminar");
-                dialog.dismiss();
-            });
             confirmacion.create().show();
-
         }catch (Exception e){
             mostrarMsgToast(e.getMessage());
 
@@ -178,11 +201,14 @@ public class MainActivity extends AppCompatActivity {
 
     //metodo agregar producto
 
-    private void agregarProductos(String accion, String [] datos ){
+    private void agregarProductos(String accion ){
         try {
             Bundle parametroProducto = new Bundle();
             parametroProducto.putString("accion",accion);
-            parametroProducto.putStringArray("datos",datos);
+            if (jsonArrayProductos.length() > 0){
+                parametroProducto.putString("datos", jsonArrayProductos.getJSONObject(position).toString());
+
+            }
             Intent nuevoProducto = new Intent(getApplicationContext(), agregarProducto.class);
             nuevoProducto.putExtras(parametroProducto);
             startActivity(nuevoProducto);
@@ -191,40 +217,102 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //Comprobar datos
+    //Obtener datos de amigos Offline
+    private void obtenerDatosOffline (){
+        try {
+            miDB = new DB(getApplicationContext(), "", null, 1 );
+            datosProductosCursos = miDB.admin_productos("consultar", null);
+            if (datosProductosCursos.moveToFirst()){
+                jsonObjectProductos = new JSONObject();
+                JSONObject jsonValueObject = new JSONObject();
+                jsonArrayProductos = new JSONArray();
+                do {
+                    jsonObjectProductos.put("_id", datosProductosCursos.getString(0));
+                    jsonObjectProductos.put("_rev", datosProductosCursos.getString(0));
+                    jsonObjectProductos.put("codigo", datosProductosCursos.getString(1));
+                    jsonObjectProductos.put("producto", datosProductosCursos.getString(2));
+                    jsonObjectProductos.put("marca", datosProductosCursos.getString(3));
+                    jsonObjectProductos.put("descripcion", datosProductosCursos.getString(4));
+                    jsonObjectProductos.put("presentacion", datosProductosCursos.getString(5));
+                    jsonObjectProductos.put("precio", datosProductosCursos.getString(6));
+                    jsonObjectProductos.put("urlPhoto", datosProductosCursos.getString(7));
+                    jsonValueObject.put("value", jsonObjectProductos);
+
+                    jsonArrayProductos.put(jsonValueObject);
+
+                }while (datosProductosCursos.moveToNext());
+                mostrarProductos();
+            } else{
+                mostrarMsgToast("Sin datos, porfavor agregue nuevos datos");
+                agregarProductos("nuevo");
+            }
+
+        }catch (Exception e){
+            mostrarMsgToast(e.getMessage());
+        }
+
+    }
+    //Obtener datos Online
+    private void obtenerDatosOnline (){
+        try {
+            serverConnection connection = new serverConnection();
+            String resp = connection.execute(u.url_cst, "GET").get();
+            jsonObjectProductos = new JSONObject(resp);
+            jsonArrayProductos = jsonObjectProductos.getJSONArray("rows");
+            mostrarProductos();
+        }catch (Exception e){
+            mostrarMsgToast(e.getMessage());
+        }
+    }
+
+    //Comprobar datos, si hay se muestran; sino se envia a agregar
 
     private void comprobardatos() {
-        miDB = new DB(getApplicationContext(),"",null,1);
-        datosProductosCursos = miDB.admin_productos("consultar",null);
-
-        if( datosProductosCursos.moveToFirst() ){
-            mostrarProductos();
-
-
-        } else {
-            mostrarMsgToast("Porfavor agregar datos");
-            agregarProductos("nuevo", new String[]{});
+        if (di.internetConnection()){
+            mostrarMsgToast("Datos desde nube");
+            obtenerDatosOnline();
+        }else {
+            jsonArrayProductos = new JSONArray();
+            mostrarMsgToast("Datos locales");
+            obtenerDatosOffline();
         }
+
     }
     //mostrar productos
     private void mostrarProductos() {
-        ltsProductos = findViewById(R.id.ltsproducotos);
-        productosArrayList.clear();
-        productosrrayListCopy.clear();
+        try {
+            if (jsonArrayProductos.length() > 0){
+                ltsProductos = findViewById(R.id.ltsproducotos);
+                productosArrayList.clear();
+                productosrrayListCopy.clear();
 
-        do{
-            misProductos = new productos(
-                    datosProductosCursos.getString(0),//idproducto
-                    datosProductosCursos.getString(1),//codigo
-                    datosProductosCursos.getString(2),//Producto
-                    datosProductosCursos.getString(3),//marca
-                    datosProductosCursos.getString(4),//descripcion
-                    datosProductosCursos.getString(5),//presentacion
-                    datosProductosCursos.getString(6), //precio
-                    datosProductosCursos.getString(7) //urldefoto
-            );
-            productosArrayList.add(misProductos);
-        }while(datosProductosCursos.moveToNext());
+                JSONObject jsonObject;
+                for (int i = 0; i < jsonArrayProductos.length(); i++){
+                    jsonObject = jsonArrayProductos.getJSONObject(i).getJSONObject("value");
+                    misProductos = new productos(
+                            jsonObject.getString("_idProducto"),
+                            jsonObject.getString("_rev"),
+                            jsonObject.getString("codigo"),
+                            jsonObject.getString("producto"),
+                            jsonObject.getString("marca"),
+                            jsonObject.getString("descripcion"),
+                            jsonObject.getString("presentacion"),
+                            jsonObject.getString("precio"),
+                            jsonObject.getString("urlPhoto")
+                    );
+                    productosArrayList.add(misProductos);
+                }
+                adaptadorImagenes adaptadorImagenes = new adaptadorImagenes(getApplicationContext(), productosArrayList);
+                ltsProductos.setAdapter(adaptadorImagenes);
+                registerForContextMenu(ltsProductos);
+                productosrrayListCopy.addAll(productosArrayList);
+            } else {
+                mostrarMsgToast("Sin registro");
+                agregarProductos("nuevo");
+            }
+        }catch (Exception e){
+            mostrarMsgToast(e.getMessage());
+        }
 
         try {
             adaptadorImagenes adaptadorImagenes = new adaptadorImagenes(getApplicationContext(), productosArrayList);
@@ -238,6 +326,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
     };
+
+    private class serverConnection extends AsyncTask<String, String, String> {
+        HttpURLConnection urlConnection;
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... pms) {
+            StringBuilder result = new StringBuilder();
+
+            try {
+                String uri = pms[0];
+                String metodo = pms[1];
+                URL url = new URL(uri);
+                urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection.setRequestMethod(metodo);
+
+                InputStream entrada = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader lectorBuffer = new BufferedReader(new InputStreamReader(entrada));
+                String line;
+
+                while ((line = lectorBuffer.readLine()) != null){
+                    result.append(line);
+                }
+
+            }catch (Exception e){
+                Log.i("GET", e.getMessage());
+            }
+            return result.toString();
+        }
+    }
 }
 
 
@@ -245,6 +367,7 @@ public class MainActivity extends AppCompatActivity {
 
 class productos{
     String idProducto;
+    String rev;
     String codigo;
     String producto;
     String marca;
@@ -254,8 +377,9 @@ class productos{
     String urlPhoto;
 
 
-    public productos(String idProducto, String codigo, String producto, String marca, String descripcion, String presentacion, String precio, String urlPhoto) {
+    public productos(String idProducto, String rev, String codigo, String producto, String marca, String descripcion, String presentacion, String precio, String urlPhoto) {
         this.idProducto = idProducto;
+        this.rev = rev;
         this.codigo = codigo;
         this.producto = producto;
         this.marca = marca;
@@ -263,6 +387,14 @@ class productos{
         this.presentacion = presentacion;
         this.precio = precio;
         this.urlPhoto = urlPhoto;
+    }
+
+    public String getRev() {
+        return rev;
+    }
+
+    public void setRev(String rev) {
+        this.rev = rev;
     }
 
     public String getIdProducto() {
--- app/src/main/java/com/ugb/miprimercalculadora/utilidades.java
+++ app/src/main/java/com/ugb/miprimercalculadora/utilidades.java
@@ -1,6 +1,6 @@
-package com.ugb.miprimercalculadora;
+    package com.ugb.miprimercalculadora;
 
 public class utilidades {
-    static String url_cst ="http://192.168.43.117:5984/db_productos/_design/productos/_view/producto";
-    static String url_mto = "http://192.168.43.117:5984/db_productos/";
+    static String url_cst ="http://192.168.8.101:5984/db_productos/_design/productos/_view/producto";
+    static String url_mto = "http://192.168.8.101:5984/db_productos/";
 }
