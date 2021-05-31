53aa129c6f1aef744ac8dfab4dbc470ab951ae08
wilsongutierrez-01 <adolfoguti001@outlook.com>
2021-05-30 23:16:44 -0600

    Arreglando error de novato

--- app/src/main/java/com/ugb/miprimercalculadora/MainActivity.java
+++ app/src/main/java/com/ugb/miprimercalculadora/MainActivity.java
@@ -20,7 +20,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RelativeLayout;
@@ -29,6 +28,13 @@ import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.auth.api.Auth;
@@ -42,6 +48,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -64,35 +71,67 @@ import java.util.List;
 import java.util.MissingFormatArgumentException;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    private CallbackManager mCallbackManeger;
     private FirebaseAuth mAuth;
     public static final int RC_SING_In = 9876;
-    private FirebaseAuth.AuthStateListener mAuthListener;
     private GoogleSignInClient mGoogleSignClien;
-
-    Button btn;
+    private LoginButton loginButton;
+    private final static String TAG = "FacebookAuthentication";
+    private FirebaseAuth.AuthStateListener authStateListener;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //Firebase Inicializacion
         mAuth = FirebaseAuth.getInstance();
+        //SDK Facebook
+        FacebookSdk.sdkInitialize(getApplicationContext());
 
         //Pantallita Google
         createResquet();
 
-        btn = findViewById(R.id.btnGoogle);
+        btn = findViewById(R.id.sign_in_button);
         btn.setOnClickListener(v -> {
             signIn();
         });
+        //Iniciar sesion Facebook
+        loginButton = findViewById(R.id.login_button);
+        mCallbackManeger = CallbackManager.Factory.create();
+        loginButton.registerCallback(mCallbackManeger, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "onSucces" + loginResult);
+                handleFacebookToken(loginResult.getAccessToken());
 
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "onSucces");
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "onErrors" + error);
+
+            }
+        });
 
 
     }
 
+    /*Desde aca empieza para iniciar Sesion con Google
+     ****************************************************************************************************************
+     ***************************************************************************************************************
+     */
+
     //Creacion de Token
-    private void createResquet(){
+    private void createResquet() {
         GoogleSignInOptions gso = new GoogleSignInOptions
                 .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -104,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Metodo SignIn
 
-    private void signIn(){
+    private void signIn() {
         Intent signIntent = mGoogleSignClien.getSignInIntent();
         startActivityForResult(signIntent, RC_SING_In);
     }
@@ -112,21 +151,22 @@ public class MainActivity extends AppCompatActivity {
     //Activity Result
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        mCallbackManeger.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == RC_SING_In){
+        if (requestCode == RC_SING_In) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 firebaseAuthWithGoogle(account);
 
-            }catch (Exception e){
+            } catch (Exception e) {
 
             }
         }
     }
 
-  //Metodo de FirabaseAuth para google
+    //Metodo de FirabaseAuth para google
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
@@ -151,6 +191,50 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+    /*
+    Termina Inicar Sesion Google
+    *******************************************************************************************************************
+    ********************************************************************************************************************
+     */
+
+    /*
+    ******************************************************************************************************
+        Iniciar Sesion Facebook
+        ****************************************************************************************************
+        ******************************************************************************************************
+     */
+
+    private void handleFacebookToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookToken" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithCredential:successful");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            Log.d(TAG, "signInWithCredential:failure");
+                            Toast.makeText(getApplicationContext(), "Error al iniciar", Toast.LENGTH_LONG);
+
+                        }
+
+                    }
+                });
+    }
+
+
+
+       /*
+    ******************************************************************************************************
+        Termina Iniciar Sesion Facebook
+        ****************************************************************************************************
+        ******************************************************************************************************
+     */
+
     //OnStar para mantener sesion iniciada
     @Override
     public void onStart() {
@@ -158,13 +242,11 @@ public class MainActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         //updateUI(currentUser);
-        if (currentUser != null){
+        if (currentUser != null) {
             Intent home = new Intent(getApplicationContext(), Home.class);
             startActivity(home);
         }
+
+
     }
-
-
-
-
 }
