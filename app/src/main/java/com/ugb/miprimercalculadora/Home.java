bc68e5120b291d256e6242143fe0ad98bae8b72d
wilsongutierrez-01 <adolfoguti001@outlook.com>
2021-06-03 22:52:45 -0600

    Profile Activity

--- app/src/main/java/com/ugb/miprimercalculadora/Home.java
+++ app/src/main/java/com/ugb/miprimercalculadora/Home.java
@@ -16,72 +16,26 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Home extends AppCompatActivity {
     private Button btn;
-    private FirebaseAuth mAuth;
-    private TextView nombre, correo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        //Inicializar Firebase
-        mAuth = FirebaseAuth.getInstance();
-
-        //Enlacer TextViews
-        correo = findViewById(R.id.txtCorreoH);
-        nombre = findViewById(R.id.txtNombreH);
-
-        //Obtener datos de usuario
-        userinfo();
-
-        btn = findViewById(R.id.btnSalir);
+        //Enviar a Profile
+        btn = findViewById(R.id.btnPerfil);
         btn.setOnClickListener(v -> {
-            cerrarSesion();
+           profile();
         });
 
 
-
     }
 
-    //Cerrar sesion
-    private void cerrarSesion(){
-        FirebaseAuth.getInstance().signOut();
-        Toast.makeText(getApplicationContext(), "Saliendo..", Toast.LENGTH_LONG).show();
-        Intent iniciarSesion = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(iniciarSesion);
+    //Enviar a Profile
+    private void profile(){
+        Intent profile = new Intent(getApplicationContext(), Profile.class);
+        startActivity(profile);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        userinfo();
-        //updateUI(currentUser);
-    }
 
-    //Obtener la informacion de los ususarios
-    private void userinfo (){
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            // Name, email address, and profile photo Url
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            Uri photoUrl = user.getPhotoUrl();
 
-            // Check if user's email is verified
-            boolean emailVerified = user.isEmailVerified();
-
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getIdToken() instead.
-            String uid = user.getUid();
-
-            nombre.setText(name);
-            correo.setText(email);
-            ImageView imageView = findViewById(R.id.imgPhoto);
-            Glide.with(getApplicationContext()).load(user.getPhotoUrl()).into(imageView);
-
-        }
-
-    }
 }
